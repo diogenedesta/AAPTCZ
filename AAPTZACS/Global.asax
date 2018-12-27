ab65ff0113f0f57de70790f1098346dc7e090baa
diogenedesta <androidismylife4@gmail.com>
2018-12-27 16:27:05 +0300

    fisrtseuo

---
+++ AAPTZACS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AAPTZACS.MvcApplication" Language="C#" %>
---
+++ AAPTZACS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AAPTZACS.Startup))]
+namespace AAPTZACS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
